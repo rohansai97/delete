e67e42f8bb0a4dc97a0f30374c831f484d8cf228
ROHAN MULAMARI <ROHAN MULAMARI@ROHAN>
2018-05-02 16:30:52 +0530

    very new files

---
+++ Logics/src/ForLoop.java
@@ -0,0 +1,8 @@
+class ForLoop {
+public static void main(String[] args) {
+	
+	for (int i =2 ; i<=5  ; ++i) {
+		System.out.println(i);
+	}
+}
+}
---
+++ Logics/src/Shifting.java
@@ -0,0 +1,14 @@
+
+public class Shifting {
+	public static void main(String[] args) {
+		System.out.println(5 << 2);// this is left shifting it comes like 5 binary value
+		// is 101 by adding 2 zeros in the right side so value comes as 10100 for that
+		// value is 20
+
+		System.out.println(20 >> 3);// in this cancel last 2 digits so 10100 becomes 101
+		// so value becomes 101 i.e, 5
+
+	}
+
+}
+
